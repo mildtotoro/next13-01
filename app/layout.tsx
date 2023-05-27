93f2c97ce2a0a5f42cb19fce453360b63aa339ae
mild pimrampha <pimarmpha@onework.dev>
2023-05-27 22:57:50 +0700

    chore: update formatted

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,15 @@ import { Kanit } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 
-
 const kanits = Kanit({
-  weight: ["300", "400"],
-  subsets: ['thai']
+  weight: ['300', '400'],
+  subsets: ['thai'],
 })
 
 export const metadata = {
   title: 'Title Media',
   description: 'Web create by next.js 13.3',
-  keyword: 'next.js, tailwind'
+  keyword: 'next.js, tailwind',
 }
 
 export default function RootLayout({
@@ -23,8 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={kanits.className}>
-        <Header/>
-        <main className='text-xl px-8'>{children}</main>
+        <Header />
+        <main className="text-xl px-8">{children}</main>
       </body>
     </html>
   )
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const loading = () => {
-  return (
-    <div className=''>
-      <div>Loading</div>
-    </div>
-  )
+    return (
+        <div className="">
+            <div>Loading</div>
+        </div>
+    )
 }
 
 export default loading
