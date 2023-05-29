e155a888ddec194d1e71dc82d23c8089befc4843
mild pimrampha <pimarmpha@onework.dev>
2023-05-29 23:09:12 +0700

    feat: integrate testing people card component

--- app/components/__tests__/PeopleCard.test.js
+++ app/components/__tests__/PeopleCard.test.js
@@ -36,4 +36,37 @@ describe('PeopleCard', () => {
     const name = screen.getByTestId('name').textContent
     expect(name).toEqual('jendi asa')
   })
+
+  it('renders a card to match snapshot', () => {
+    const { asFragment } = render(
+      <PeopleCard
+        data={{
+          id: 'b66231c5-3f2c-4997-91ca-49aa42b8781f',
+          name: 'hello',
+          height: '150',
+          mass: '49',
+          hair_color: 'brown',
+          skin_color: 'light',
+          eye_color: 'brown',
+          birth_year: '19BBY',
+          gender: 'female',
+          homeworld: 'https://swapi.dev/api/planets/2/',
+          films: [
+            'https://swapi.dev/api/films/1/',
+            'https://swapi.dev/api/films/2/',
+            'https://swapi.dev/api/films/3/',
+            'https://swapi.dev/api/films/6/',
+          ],
+          species: [],
+          vehicles: ['https://swapi.dev/api/vehicles/30/'],
+          starships: [],
+          created: '2014-12-10T15:20:09.791000Z',
+          edited: '2014-12-20T21:17:50.315000Z',
+          url: 'https://swapi.dev/api/people/5/',
+        }}
+      />
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
 })
